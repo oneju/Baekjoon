0a182cba4a83ffbc09ed1178ea52f929922386db
wonju <wonju.noh.24@gmail.com>
2023-04-12 20:22:17 +0900

    기초 (배열)

--- week_1st/1_16(4344).py
+++ week_1st/1_16(4344).py
@@ -4,16 +4,13 @@ for i in range(0,C):
     case = input().split(" ")
     n = int(case[0])
     sum=0
-    score_lst = []
     for j in range(0,n):
-        score_lst.append(int(case[j+1]))
-        sum+=score_lst[j]
+        sum+=int(case[j+1])
     avg = sum/int(n)
     cnt =0
-    for score in score_lst:
+    for score in case[1:n]:
         if score > avg:
             cnt+=1
-    per = cnt/n * 100
-    print("{:.3f}%".format(per))
+    print("{:.3f}%".format(cnt/n * 100))
     
     
