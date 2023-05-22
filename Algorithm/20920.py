ec5e80611b8f0f77051c5736a1711066b05be16c
Wonju Noh <wonju@Wonjuui-MacBookAir.local>
2023-05-22 13:09:24 +0900

    영단어 암기는 괴로워

---
+++ Algorithm/20920.py
@@ -0,0 +1,14 @@
+# 영단어 암기는 괴로워
+# https://www.acmicpc.net/problem/20920
+import sys
+input = sys.stdin.readline
+n,m = map(int,input().split())
+mem = {}
+for _ in range(n):
+    word = input().strip()
+    if len(word) >= m:
+        if word in mem:mem[word]+=1
+        else:mem[word] = 1
+print()
+mem = sorted(mem.items(), key=lambda x:(-x[1],-len(x[0]),x[0]))
+[print(word[0])for word in mem]
