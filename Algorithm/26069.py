f6ddba757bbbe378403025fd61442fc92216bc29
Wonju Noh <wonju@Wonjuui-MacBookAir.local>
2023-05-25 09:54:06 +0900

    붙임성 좋은 총총이 V.2

--- Algorithm/26069.py
+++ Algorithm/26069.py
@@ -3,20 +3,13 @@
 import sys
 input = sys.stdin.readline
 n = int(input())
-chong = {}
+chong = {"ChongChong":1}
 ans = 0
 for _ in range(n):
     a,b = input().split()
-    if "ChongChong" in [a,b]:
-        chong[a] = 1
-        chong[b] = 1
-    elif a in chong and chong[a]==1:
+    if a in chong:
         chong[b]=1
-    elif b in chong and chong[b]==1:
+    elif b in chong:
         chong[a]=1
-    else:
-        chong[a]=0
-        chong[b]=0
-for a in chong:
-    if chong[a] == 1:ans+=1
+for a in chong:ans+=1
 print(ans)
