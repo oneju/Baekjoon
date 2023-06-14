c60619a729b08451b1b8a1c71f940ae604594633
Wonju Noh <wonju@Wonjuui-MacBookAir.local>
2023-06-14 09:31:15 +0900

    dfs/bfs ( 적록 색약 )

--- Algorithm/10026.py
+++ Algorithm/10026.py
@@ -10,10 +10,13 @@ def dfs(x,y,c):
     visit[x][y] = 1
     xi = [0,0,1,-1]
     yi = [-1,1,0,0]
+    no = 0
     for i in range(4):
         if x+xi[i] in range(N) and y+yi[i] in range(N):
             if color_lst[x+xi[i]][y+yi[i]] == c and visit[x+xi[i]][y+yi[i]]==0:
                 dfs(x+xi[i],y+yi[i],c)
+                no = 1
+    if no==0:return
 cnt_a = cnt_b = 0
 visit = [[0]*N for _ in range(N)]
 for i in range(N):
