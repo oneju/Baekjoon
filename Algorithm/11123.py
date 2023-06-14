ec88ae0ed59a7a4c1aa1d172cd1a16a3878c9d32
Wonju Noh <wonju@Wonjuui-MacBookAir.local>
2023-06-14 09:40:47 +0900

    dfs/bfs ( 양 한마리... 양 두마리... ver.2 )

--- Algorithm/11123.py
+++ Algorithm/11123.py
@@ -1,31 +1,43 @@
 # 양 한마리... 양 두마리...
 # https://www.acmicpc.net/problem/11123
 import sys
+sys.setrecursionlimit(10**6)
+
 input = sys.stdin.readline
 T = int(input())
+
+xi = [0,0,1,-1]
+yi = [1,-1,0,0]
+# stack + while loop
+def find_s(x,y):
+    stack = []
+    stack.append([x,y])
+    f[x][y] = '.'
+    while stack:
+        xx,yy = stack.pop()
+        for a in range(4):
+            if xx+xi[a] in range(h) and yy+yi[a] in range(w):
+                if f[xx+xi[a]][yy+yi[a]] == "#":
+                    stack.append([xx+xi[a],yy+yi[a]])
+                    f[xx+xi[a]][yy+yi[a]] = '.'
+# recursion (error -> set limit)
+def find_r(x,y):
+    f[x][y] = "."
+    no = 0
+    for a in range(4):
+        if x+xi[a] in range(h) and y+yi[a] in range(w):
+            if f[x+xi[a]][y+yi[a]] == "#":
+                find_r(x+xi[a],y+yi[a])
+                no = 1
+    if no == 0: return
+
 for _ in range(T):
     h,w = map(int,input().split())
     f = [list(input()) for _ in range(h)]
-    print(f)
-    xi = [0,0,1,-1]
-    yi = [1,-1,0,0]
-    visit = [[0]*w for _ in range(h)]
-    def find(x,y):
-        stack = []
-        stack.append([x,y])
-        visit[x][y] = 1
-        while stack:
-            xx,yy = stack.pop()
-            for a in range(4):
-                if xx+xi[a] in range(h) and yy+yi[a] in range(w):
-                    if visit[xx+xi[a]][yy+yi[a]] == 0 and f[xx+xi[a]][yy+yi[a]] == "#":
-                        stack.append([xx+xi[a],yy+yi[a]])
-                        visit[xx+xi[a]][yy+yi[a]] = 1
-
     cnt=0
     for i in range(h):
         for j in range(w):
-            if visit[i][j] == 0 and f[i][j] == "#":
-                find(i,j)
+            if f[i][j] == "#":
                 cnt+=1
+                find_r(i,j)
     print(cnt)
