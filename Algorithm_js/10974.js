a9e942abb27e8baf972b853375906b75c8b4a84d
Wonju Noh <wonju@Wonjuui-MacBookAir.local>
2023-07-10 15:23:04 +0900

    완전 탐색 ( 모든 순열 )

---
+++ Algorithm_js/10974.js
@@ -0,0 +1,23 @@
+/* 모든순열
+ * https://www.acmicpc.net/problem/10974
+ */
+let input = require('fs').readFileSync('example.txt').toString().trim();
+// let input = require('fs').readFileSync('/dev/stdin').toString().trim();
+let arr = [];
+for (let i=0;i<input;i++){
+    arr.push(i+1);
+}
+const perm = (list, len) => {
+    if (len == input){
+        console.log(list.join(' '));
+        return
+    }
+    for (let i=0;i<input;i++){
+        if (!list.includes(arr[i])){
+            list.push(arr[i]);
+            perm(list,len+1);
+            list.pop();
+        }
+    }
+}
+perm([],0);
