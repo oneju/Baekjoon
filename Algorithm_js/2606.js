55a7905ad5b840620a7b5d43693d2a9dffb2524a
oneju <wonju.noh.24@gmail.com>
2023-10-21 13:03:00 +0900

    바이러스 (dfs)

---
+++ Algorithm_js/2606.js
@@ -0,0 +1,26 @@
+/* 바이러스
+https://www.acmicpc.net/problem/2606 */
+let input = require("fs")
+  .readFileSync(__dirname + "/example.txt")
+  .toString()
+  .split("\n");
+// let input = require('fs').readFileSync('/dev/stdin').toString().split('\n');
+const networks = Array.from(Array(+input[0]), () => []);
+for (let i = 0; i < input[1]; i++) {
+  const [a, b] = input[i + 2].split(" ").map((a) => +a);
+  networks[a - 1].push(b - 1);
+  networks[b - 1].push(a - 1);
+}
+const visited = Array(+input[0]).fill(0);
+let cnt = 0;
+const dfs = (com) => {
+  for (const next of networks[com]) {
+    visited[next] = 1;
+    if (visited[next] === 0) {
+      cnt += 1;
+      dfs(next);
+    }
+  }
+};
+dfs(0);
+console.log(cnt);
